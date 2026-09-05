c8ed582509595d8c73460aef92d9bc04fcd8b710
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-09-05 15:27:34 +0300

    Temp: Added parser debugging code

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/lexer.h"
+#include "include/parser.h"
 #include "include/error.h"
 #include "include/utils.h"
 
@@ -29,6 +30,7 @@ int main(int argc, char *argv[]) {
     token *tokens = lexer(buffer, &tokenCount);
     free(buffer);
 
+    //lexer debug
     printf("--- USM Lexer Debug ---\n");
         for (size_t i = 0; i < tokenCount; i++) {
             printf("Line: %u | Type: %-12s | Value: '%s'\n" 
@@ -37,8 +39,23 @@ int main(int argc, char *argv[]) {
                 ,tokens[i].value
             );
         }
+
+    // pasrser debug
+    printf("\n--- USM Parser Debug ---\n");
+        size_t instrCount = 0;
+        Instruction *instructions = parser(tokens, tokenCount, &instrCount);
+        
+        for (size_t i = 0; i < instrCount; i++) {
+             printf("Line: %d | Opcode: %-6s | DestType: %d | SrcType: %d\n",
+                instructions[i].line,
+                tokenTypeToString(instructions[i].opcode),
+                instructions[i].dest.type,
+                instructions[i].src.type
+            );
+         }
     
     // stops da mem leak during dev for now later will free da tokens after its parsered
+    free(instructions);
     free(tokens);
     return 0;
 }
