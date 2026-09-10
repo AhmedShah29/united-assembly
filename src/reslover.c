6f800f13d3d3e450372c8b308c44f0605ac1ad7d
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-09-10 17:18:30 +0300

    Updated the labels/vars check thru hashmap

--- src/reslover.c
+++ src/reslover.c
@@ -1,19 +1,28 @@
+#include <limits.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include "include/error.h"
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/resolver.h"
+#include "include/hashmap.h"
 
-
-bool is_defined(const Instruction *instr, size_t instrCount, const char *name, tokenType opcode) {
-    for(size_t i = 0; i < instrCount; i++) {
-        if(instr[i].opcode == opcode && strcmp(instr[i].src.val.name, name) == 0) { return true; }
-    }
-    return false;
-}
 
 void Resolver(const Instruction *instrutions, size_t InstrCount) {
+
+    HashMap map;
+    HashMap_init(&map);
+
+    for(size_t i = 0; i < InstrCount; i++){
+        switch(instrutions[i].opcode){
+            case TOKEN_LABEL:
+                HashMap_insert(&map, instrutions[i].src.val.name, i, TYPE_LABEL);
+                break;
+            case TOKEN_ID:
+                HashMap_insert(&map, instrutions[i].src.val.name, i, TYPE_VAR);
+        }
+    }
+    
     for(size_t i = 0; i < InstrCount; i++){
         switch(instrutions[i].opcode){
             case TOKEN_JMP:
@@ -22,28 +31,28 @@ void Resolver(const Instruction *instrutions, size_t InstrCount) {
             case TOKEN_JIG:
             case TOKEN_JIL:
                 if(instrutions[i].src.type == OPERAND_LABEL){
-                    if(!is_defined(instrutions, InstrCount, instrutions[i].src.val.name, TOKEN_LABEL)){
+                    if(HashMap_get(&map, instrutions[i].src.val.name, TYPE_LABEL) == UINT32_MAX) {
                         UsmError("at line %d\nlabel '%s' is not defind", instrutions[i].line, instrutions[i].src.val.name);
                     }
                 }
                 break;
             case TOKEN_LOAD:
-                if(instrutions[i].dest.type == OPERAND_MEM) {
-                    if(!is_defined(instrutions, InstrCount, instrutions[i].dest.val.name, TOKEN_ID)){
+                if(instrutions[i].dest.type == OPERAND_VAR) {
+                    if(HashMap_get(&map, instrutions[i].dest.val.name, TYPE_VAR) == UINT32_MAX){
                         UsmError("at line %d\nvariable '%s' is not defined", instrutions[i].line, instrutions[i].dest.val.name);
                     }
                 }
                 break;
             case TOKEN_STR:
-                if(instrutions[i].src.type == OPERAND_MEM) {
-                    if(!is_defined(instrutions, InstrCount, instrutions[i].src.val.name, TOKEN_ID)){
+                if(instrutions[i].src.type == OPERAND_VAR) {
+                    if(HashMap_get(&map, instrutions[i].src.val.name, TYPE_VAR) == UINT32_MAX){
                         UsmError("at line %d\nvariable '%s' is not defined", instrutions[i].line, instrutions[i].src.val.name);
                     }
                 }
                 break;
             case TOKEN_CALL:
                 if(instrutions[i].src.type == OPERAND_LABEL) {
-                    if(!is_defined(instrutions, InstrCount, instrutions[i].src.val.name, TOKEN_LABEL)) {
+                    if(HashMap_get(&map, instrutions[i].src.val.name, TYPE_LABEL) == UINT32_MAX) {
                         UsmError("at line %d\nfunction '%s' is not defined", instrutions[i].line, instrutions[i].src.val.name);
                     }
                 }
